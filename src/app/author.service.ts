7b473ff7ab0b9a4bc941d3994f824a7f01bcbad7
NPI <70711818+YesLife@users.noreply.github.com>
2022-05-19 10:48:03 +0200

    Comment unused route for service Author

--- src/app/author.service.ts
+++ src/app/author.service.ts
@@ -8,22 +8,23 @@ import { Author, CreateAuthor } from './author/author.component';
 })
 export class AuthorService {
 
-  public getAuthors(): Observable<Author[]> {
-    return this.http.get<Author[]>("http://localhost:3000/authors");
-  }
+  private baseUrl = "https://my-json-server.typicode.com/Polytech-Paris-Sud-Web/TP2-NathanPinault";
+
+  /*public getAuthors(): Observable<Author[]> {
+    return this.http.get<Author[]>("${this.baseUrl}/authors");
+  }*/
 
   public getAuthor(name : string | undefined): Observable<Author[]> {
-    console.log(`http://localhost:3000/authors?name=${name}`);
-    return this.http.get<Author[]>(`http://localhost:3000/authors?name=${name}`);
+    return this.http.get<Author[]>(`${this.baseUrl}/authors?name=${name}`);
   }
 
-  public deleteAuthor(name : string | undefined): Observable<any> {
-    return this.http.delete(`http://localhost:3000/authors?name=${name}`);
+  /*public deleteAuthor(name : string | undefined): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/authors?name=${name}`);
   }
 
   public createAuthor(author: CreateAuthor): Observable<any> {
-    return this.http.post("http://localhost:3000/authors", author);
-  }
+    return this.http.post(`${this.baseUrl}/authors`, author);
+  }*/
 
   constructor(private readonly http : HttpClient) { }
 }
